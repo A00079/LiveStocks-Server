c219ffff9fcec59f14e2f6389b49cd3aeb929415
A00079 <bajay0007@gmail.com>
2020-11-28 15:02:38 +0530

    Bug fix

--- index.js
+++ index.js
@@ -9,15 +9,11 @@ const port = process.env.PORT || 5000;
 // If Production Mode On
 if (process.env.NODE_ENV === 'production') {
 
-  app.use('/',express.static(path.join(__dirname, 'client/build')));
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.use(express.static(path.join(__dirname, 'build')));
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
-  
-  app.use("/portal",express.static(path.join(__dirname, 'portal/build')));
-  app.get("/portal/*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'portal/build', 'index.html'));
-  });
+
 }
 
 app.listen(port, () => {
